f5f6298eb71c1ef81bc7caf5ab47587231fed649
elinsecl <lynxjosue@gmail.com>
2025-06-22 16:10:05 -0600

    asdf

--- Restaurante/src/App.jsx
+++ Restaurante/src/App.jsx
@@ -3,15 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import IngredientePage from './components/ingrediente/IngredientePage';
-import { Categoria } from './components/categoria';
-import { Platillo } from './components/platillo';
+
 
 function App() {
   return (
     <div>
       <IngredientePage />
-      <Categoria />
-      <Platillo />
     </div>
   );
 }
